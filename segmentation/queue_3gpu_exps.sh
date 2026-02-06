a8ed5d44f36ad9438788ad86357897c9ca7f0c30
cwk <a744894570@outlook.com>
2026-02-06 18:42:50 +0800

    减少训练

--- segmentation/queue_3gpu_exps.sh
+++ segmentation/queue_3gpu_exps.sh
@@ -11,7 +11,7 @@ BASE_LR=4e-5
 
 queue_base() {
     local dataset=$1
-    dvc exp run --queue \
+    dvc exp run --queue --force \
         --name "${dataset}_base_g${GPUS}_bs${BATCH}_cs416" \
         -S dataset=${dataset} \
         -S train.gpus=${GPUS} \
@@ -22,7 +22,7 @@ queue_base() {
 
 queue_acc() {
     local dataset=$1
-    dvc exp run --queue \
+    dvc exp run --queue --force \
         --name "${dataset}_lr2e-5_g${GPUS}_bs${BATCH}_cs416" \
         -S dataset=${dataset} \
         -S train.gpus=${GPUS} \
@@ -30,15 +30,15 @@ queue_acc() {
         -S "train.crop_size=${CROP}" \
         -S train.optimizer.lr=2e-5
 
-    dvc exp run --queue \
-        --name "${dataset}_lr6e-5_g${GPUS}_bs${BATCH}_cs416" \
-        -S dataset=${dataset} \
-        -S train.gpus=${GPUS} \
-        -S train.batch_size=${BATCH} \
-        -S "train.crop_size=${CROP}" \
-        -S train.optimizer.lr=6e-5
+    # dvc exp run --queue --force \
+    #     --name "${dataset}_lr6e-5_g${GPUS}_bs${BATCH}_cs416" \
+    #     -S dataset=${dataset} \
+    #     -S train.gpus=${GPUS} \
+    #     -S train.batch_size=${BATCH} \
+    #     -S "train.crop_size=${CROP}" \
+    #     -S train.optimizer.lr=6e-5
 
-    dvc exp run --queue \
+    dvc exp run --queue --force \
         --name "${dataset}_nq100_g${GPUS}_bs${BATCH}_cs416" \
         -S dataset=${dataset} \
         -S train.gpus=${GPUS} \
